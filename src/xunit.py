8d1b61c333f3b838892553c58d27e12053a7d7f3
RichterAstell <xparis1721x@gmail.com>
2021-03-31 23:39:21 +0900

    fix

--- src/xunit.py
+++ src/xunit.py
@@ -1,8 +1,18 @@
-class WasRun:
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        super().__init__(name)
+    def testMethod(self):
+        self.wasRun = 1
 
 test = WasRun("testMethod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
