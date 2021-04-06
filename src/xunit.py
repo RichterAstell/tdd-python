9adc7e4e3a28654dc2f20d4acd0fe2636fe92b5e
RichterAstell <xparis1721x@gmail.com>
2021-04-07 00:43:10 +0900

    chapter 20

--- src/xunit.py
+++ src/xunit.py
@@ -7,26 +7,26 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+    def tearDown(self):
+        pass
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-    def testRunning(self):
-        self.test.run()
-        assert(test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert(test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
+# TestCaseTest("testSetUp").run()
 # test = WasRun("testMethod")
 # print(test.wasRun)
 # test.run()
