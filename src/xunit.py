cf8855a7146e0aa128624c60f26bc6523399f26e
RichterAstell <xparis1721x@gmail.com>
2021-04-06 23:58:42 +0900

    chapter 19

--- src/xunit.py
+++ src/xunit.py
@@ -9,23 +9,20 @@ class TestCase:
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
+        self.test.run()
         assert(test.wasRun)
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
+        self.test.run()
         assert(test.wasSetUp)
 
 TestCaseTest("testRunning").run()
@@ -34,3 +31,8 @@ TestCaseTest("testSetUp").run()
 # print(test.wasRun)
 # test.run()
 # print(test.wasRun)
+
+# classの整理
+# TestCaseTest - テスト対象を選択して run() でテストを呼び出す
+# WasRun - テスト対象？
+# TestCase - テストケースのベースクラス
