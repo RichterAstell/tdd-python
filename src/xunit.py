69f94e741315f376c5386cf7da2164add41bf098
RichterAstell <xparis1721x@gmail.com>
2021-04-12 01:09:10 +0900

    chapter 22

--- src/xunit.py
+++ src/xunit.py
@@ -7,9 +7,13 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
+        print("err: %d" % result.errorCount)
         return result
     def tearDown(self):
         pass
@@ -17,10 +21,13 @@ class TestCase:
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
     def testStarted(self):
         self.runCount = self.runCount + 1
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
 
 class WasRun(TestCase):
     def setUp(self):
@@ -41,14 +48,20 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         result = test.run()
         assert("1 run, 0 failed" == result.summary())
+    def testResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
         assert("1 run, 1 failed" == result.summary())
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-# TestCaseTest("testFailedResult").run()
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
+print(TestCaseTest("testResultFormatting").run().summary())
 
 # TestCaseTest("testSetUp").run()
 # test = WasRun("testMethod")
